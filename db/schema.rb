d7563a7cb7d449e66878307041ba7e52d21e0397
Masataka Okada <masataka.okada1117@gmail.com>
2020-01-13 19:16:03 +0900

    studioモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_14_150546) do
+ActiveRecord::Schema.define(version: 2020_01_12_084014) do
+
+  create_table "studios", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
