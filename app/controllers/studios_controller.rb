50f016fcc0797eb7d7c9f920d1d93237b4503fd0
Masataka Okada <masataka.okada1117@gmail.com>
2020-05-16 16:08:33 +0900

    スタジオ詳細ページの作成

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,5 +1,16 @@
 class StudiosController < ApplicationController
+  before_action :set_studio, only: %i[show]
+
   def index
     @studios = Studio.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_studio
+    @studio = Studio.find(params[:id])
   end
 end
