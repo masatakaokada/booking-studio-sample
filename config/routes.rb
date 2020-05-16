f2429bdd779eef860cc8b4590e28e300e8e86709
Masataka Okada <masataka.okada1117@gmail.com>
2020-05-16 13:25:58 +0900

    スタジオ一覧ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   # トップページ
-  root "top#index"
+  root "studios#index"
 
   resources :studios
 end
