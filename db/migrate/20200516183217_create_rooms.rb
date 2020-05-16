84a58d2815549742d9a9b0dc4f4804f567628cbe
Masataka Okada <masataka.okada1117@gmail.com>
2020-05-17 04:10:14 +0900

    外部キー制約の追加

--- db/migrate/20200516183217_create_rooms.rb
+++ db/migrate/20200516183217_create_rooms.rb
@@ -2,6 +2,7 @@ class CreateRooms < ActiveRecord::Migration[5.2]
   def change
     create_table :rooms do |t|
       t.string :name, null: false
+      t.references :studio, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_12_084014) do
+ActiveRecord::Schema.define(version: 2020_05_16_183217) do
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "studio_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["studio_id"], name: "index_rooms_on_studio_id"
+  end
 
   create_table "studios", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -36,4 +44,5 @@ ActiveRecord::Schema.define(version: 2020_01_12_084014) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "rooms", "studios"
 end
