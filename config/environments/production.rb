a3603c6e385b693fa5a96f4bd884a2106a456c7c
Masataka Okada <masataka.okada1117@gmail.com>
2020-06-15 09:14:53 +0900

    ログローテートの記述追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # ログローテート
+  config.logger = Logger.new('log/production.log', 'weekly')
 end
